a4e4103f29799c41d61153d327c7be8e83808c61
Jacob Chademwiri <jchademwiri@gmail.com>
2026-03-03 11:19:31 +0200

    bulldozzer price update

--- src/components/PlantHireCalculator/utils/constants.ts
+++ src/components/PlantHireCalculator/utils/constants.ts
@@ -4,7 +4,7 @@ export const EQUIPMENT_PRESETS: EquipmentPreset[] = [
   { name: 'Dropside', rate: 5200.00 },
   { name: 'ADT', rate: 5913.04 },
   { name: 'FEL', rate: 4769.57 },
-  { name: 'Bulldozzer', rate: 660.00 },
+  { name: 'Bulldozzer', rate: 6360.00 },
   { name: 'Skid Steer', rate: 2434.78 },
   { name: 'Concrete Cutter', rate: 415.88 },
 ];
